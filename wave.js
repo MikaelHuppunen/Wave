27ce02b1785bb229eede8d218bef5e4877519f69
MikaelHuppunen <98363447+MikaelHuppunen@users.noreply.github.com>
2024-01-24 21:55:45 +0200

    Updated proportions

--- wave.js
+++ wave.js
@@ -1,9 +1,17 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-let radius = 50;
+let person_height = 400;
+let head_radius = person_height/16;
+let leg_length = person_height/4;
+let shin_length = person_height/4;
+let arm_length = 1.67/8*person_height;
+let forearm_length = 1/8*person_height;
+let torso_length = 2.67/8*person_height;
+let neck_length = 0.33/8*person_height;
+let hand_length = 0.75/8*person_height;
 let x = canvas.width / 2;
-let y = canvas.height / 2;
+let y = canvas.height / 4;
 
 function draw_line(start_x, start_y, end_x, end_y){
     ctx.beginPath();
@@ -18,15 +26,24 @@ function draw(){
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+    ctx.arc(x, y, head_radius, 0, Math.PI * 2, true);
     ctx.stroke();
     ctx.closePath();
 
-    draw_line(x, y+radius, x, y+radius+100);
-    draw_line(x, y+radius+10, x+radius, y+radius+100);
-    draw_line(x, y+radius+10, x-radius, y+radius+100);
-    draw_line(x, y+radius+100, x+radius, y+radius+200);
-    draw_line(x, y+radius+100, x-radius, y+radius+200);
+    draw_line(x, y+head_radius, x, y+head_radius+neck_length);
+    draw_line(x, y+head_radius+neck_length, x, y+head_radius+neck_length+torso_length);
+    //arms
+    draw_line(x, y+head_radius+neck_length, x+head_radius, y+head_radius+neck_length+arm_length);
+    draw_line(x, y+head_radius+neck_length, x-head_radius, y+head_radius+neck_length+arm_length);
+    draw_line(x+head_radius, y+head_radius+neck_length+arm_length, x+head_radius, y+head_radius+neck_length+arm_length+forearm_length);
+    draw_line(x-head_radius, y+head_radius+neck_length+arm_length, x-head_radius, y+head_radius+neck_length+arm_length+forearm_length);
+    draw_line(x+head_radius, y+head_radius+neck_length+arm_length+forearm_length, x+head_radius, y+head_radius+neck_length+arm_length+forearm_length+hand_length);
+    draw_line(x-head_radius, y+head_radius+neck_length+arm_length+forearm_length, x-head_radius, y+head_radius+neck_length+arm_length+forearm_length+hand_length);
+    //legs
+    draw_line(x, y+head_radius+neck_length+torso_length, x+head_radius, y+head_radius+neck_length+torso_length+leg_length);
+    draw_line(x, y+head_radius+neck_length+torso_length, x-head_radius, y+head_radius+neck_length+torso_length+leg_length);
+    draw_line(x+head_radius, y+head_radius+neck_length+torso_length+leg_length, x+head_radius, y+head_radius+neck_length+torso_length+leg_length+shin_length);
+    draw_line(x-head_radius, y+head_radius+neck_length+torso_length+leg_length, x-head_radius, y+head_radius+neck_length+torso_length+leg_length+shin_length);
 }
 
 setInterval(draw, 10);
